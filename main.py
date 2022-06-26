8728f844ddfa52ae23329928f220d32c24be8759
Mariusz <mariuszbrozda.dublin@gmail.com>
2022-06-26 20:58:53 +0100

    Add functionality to send SMS messages with NIO  stock news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 
-import pandas
 import requests
 import os
 from os import environ
@@ -9,25 +8,16 @@ from twilio.http.http_client import TwilioHttpClient
 import datetime as dt
 
 
-
 NIO_STOCK = "NIO"
-OIL_STOCK = 'OIL'
-BERKSHIRE = 'BRK.B'
-COMPANY_NAME = "Tesla Inc"
-
-
-STOCK_NAME=[NIO_STOCK, OIL_STOCK, BERKSHIRE ]
+STOCK_NAME=  NIO_STOCK
 
 # STOCK MARKET
-stock_api = ""
-stock_endpoint = "https://www.alphavantage.co/query"
-
-
+stock_api = os.environ.get('STOCK_API')
+stock_endpoint = os.environ.get('STOCK_ENDPOINT')
 
 stock_params = {
     'function': 'TIME_SERIES_DAILY',
-    'symbol': STOCK_NAME[0],
-    # 'interval': '60min',
+    'symbol': NIO_STOCK,
     'apikey': stock_api,
 
 }
@@ -38,37 +28,31 @@ oil_stock_response = requests.get(url=stock_endpoint, params=stock_params)
 
 nio_data = nio_stock_response.json()["Time Series (Daily)"]
 
-
 data = [ value for (key,value) in nio_data.items() ]
-
 nio_yesterday_data = data[0]
 nio_day_before_data = data[1]
 
-print(data)
-print(nio_yesterday_data['4. close'])
-print(nio_day_before_data['4. close'])
 
 price_diff = abs(float(nio_yesterday_data['4. close']) - float(nio_day_before_data['4. close']))
 
 price_percentage_diff = (price_diff / float(nio_yesterday_data['4. close'])) * 100
 
 price_rounded = round(price_percentage_diff, 2)
-print(price_rounded)
-
 
 up_down = None
-if price_rounded > 0:
+if abs(price_rounded) > 0:
     up_down = "🔺"
 else:
     up_down = "🔻"
 
+
 # GETTING DATA FROM NEWS API
-news_api =''
-news_endpoint = 'https://newsapi.org/v2/everything'
+news_api = os.environ.get('NEWS_API')
+news_endpoint = os.environ.get('NEWS_ENDPOINT')
 
 print(nio_data)
 news_params = {
-    'q': 'NIO STOCK',
+    'qInTitle': 'NIO',
     'published': nio_yesterday_data,
     'apikey': news_api,
     'language': 'en',
@@ -82,28 +66,23 @@ nio_news = news_response.json()
 articles = nio_news['articles']
 
 three_articles = articles[:3]
-print(three_articles)
 
-formatted_articles = [f"{STOCK_NAME}: {up_down}{price_rounded}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
-print(formatted_articles)
+formatted_articles = [f"{STOCK_NAME}: {up_down} {price_rounded} % \n \n Headline: {article['title']}. \n Brief: {article['description']} \n" for article in three_articles]
 
 
+# SEND SMS MESSAGE WITH NEWS AND NIO STOCK PRICE INFO
 
-#
-# twillio_account_sid = ""
-#
-# twillio_auth_token = ""
-#
-#
-#
-# client = Client(twillio_account_sid, twillio_auth_token)
-# nio_art_description = articles[:]['description']
-# message_body = f''
-# message = client.messages \
-#         .create(
-#             body=f"STOCK NEWS! {nio_art_description} \n ",
-#             from_='',
-#             to=''
-#         )
-# print(message.sid)
+twillio_account_sid = os.environ.get('TWILLIO_ACC_SID')
+twillio_auth_token = os.environ.get('TWILLIO_TOKEN')
+
+client = Client(twillio_account_sid, twillio_auth_token)
+
+for article in formatted_articles:
+    message = client.messages \
+            .create(
+                body=f"{article} \n ",
+                from_= os.environ.get('TWILLIO_PHONE_NR'),
+                to= os.environ.get('RECEIVER_NUMBER')
+            )
+print(message.sid)
 
